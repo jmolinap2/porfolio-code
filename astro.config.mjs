525c90e9ea4935b25be2cd00be8eabe8a65383cf
Jesus M <jmolinap2@unemi.edu.ec>
2025-05-20 06:27:22 -0500

    depoy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://jmolina2.github.io/porfolio-code/',
+  base: '/porfolio-code/'
 })
---
+++ src/utils/constants.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = import.meta.env.BASE_URL;
+export const IMAGES_PATH = `${BASE_URL}img/`; 
+export const PROJECTS_PATH = `${BASE_URL}projects/`;
+export const FAVICON_PATH = `${BASE_URL}favicon.svg`;

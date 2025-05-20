d7bf1541a127cd66c450e96896a7315fabca6dc0
Jesus M <jmolinap2@unemi.edu.ec>
2025-05-20 14:23:42 -0500

    depoy netfily

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from '@astrojs/tailwind'
 import robotsTxt from 'astro-robots-txt'
 
 export default defineConfig({
-  site: 'https://jmolinap2.github.io/porfolio-code/',
-  base: '/porfolio-code/',
-  integrations: [tailwind(), robotsTxt()],
-})
+  site: 'https://jmolinap2.github.io/',
+ integrations: [tailwind(), robotsTxt()],
+});

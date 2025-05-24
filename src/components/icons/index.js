5b1bf6846649294680abdcc494c24bc1d9b7a9e8
Jesus M <jmolinap2@unemi.edu.ec>
2025-05-24 10:10:32 -0500

    depoy netfily mejoras

---
+++ src/components/icons/index.js
@@ -0,0 +1,26 @@
+import Angular from './Angular.astro';
+import CSharp from './CSharp.astro';
+import SqlServer from './SqlServer.astro';
+import Nodejs from './Nodejs.astro';
+import Postgresql from './Postgresql.astro';
+import Django from './Django.astro';
+import Python from './Python.astro';
+import Bootstrap from './Bootstrap.astro';
+import Oracle from './Oracle.astro';
+import Git from './Git.astro';
+import TypeScript from './TypeScript.astro'
+
+
+export const ICONS = {
+  Django,
+  Python,
+  Angular,
+  'SQL Server': SqlServer,
+  PostgreSQL: Postgresql,
+  'C# .NET': CSharp,
+  'Node.js': Nodejs,
+  Bootstrap,
+  Oracle,
+  Git,
+  TypeScript,
+};
